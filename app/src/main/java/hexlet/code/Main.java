46a405f870ac8372a95ea0215f19cdeca9c70c5f
stronty <strontium8762aem@gmail.com>
2026-06-15 09:11:40 +0300

    add temporary tests

--- app/src/main/java/hexlet/code/Main.java
+++ app/src/main/java/hexlet/code/Main.java
@@ -1,13 +1,37 @@
 package hexlet.code; // main package
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import schemas.Schema;
+
+import java.util.HashMap;
+import java.util.Map;
+
 class Main {
     void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/>
-        // with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
 
+
+        var v = new Validator();
+
+        var schema = v.map();
+
+        Map<String, Schema<String>> schemas = new HashMap<>();
+
+        schemas.put("firstName", v.string().required());
+        schemas.put("lastName", v.string().required().minLength(2));
+
+        schema.shape(schemas);
+        Map<String, String> human1 = new HashMap<>();
+        human1.put("firstName", "John");
+        human1.put("lastName", "Smith");
+        schema.isValid(human1); // true
+
+        Map<String, String> human2 = new HashMap<>();
+        human2.put("firstName", "John");
+        human2.put("lastName", null);
+        schema.isValid(human2); // false
+
+        Map<String, String> human3 = new HashMap<>();
+        human3.put("firstName", "Anna");
+        human3.put("lastName", "B");
+        schema.isValid(human3); // false
     }
 }
